452d59c078fd5a64d46060b21e8be3601a06f913
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-11 22:05:02 +0300

    commit#2

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -3,7 +3,9 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -11,10 +13,15 @@ public class Main extends Game {
 
 	SpriteBatch batch;
 	BitmapFont font;
+	Texture fon;
 
 	@Override
 	public void create () {
-
+		batch = new SpriteBatch();
+		fon = new Texture(Gdx.files.internal("fon.jpg"));
+		font = new BitmapFont();
+		font.setColor(Color.RED);
+		this.setScreen(new MenuScreen(this));
 	}
 
 	@Override
