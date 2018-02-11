451d8d7d4150a0c7a1892eb05058d680f304ebcb
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-11 21:40:06 +0300

    commit#1

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -1,54 +1,30 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 
-public class Main extends ApplicationAdapter {
-	Control control;
+public class Main extends Game {
+
 	SpriteBatch batch;
-	Model model;
-	Texture myFon;
-	Texture myOk;
+	BitmapFont font;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		model = new Model(20, 4);
-		myFon = new Texture("fon.jpg");
-		myOk = new Texture("ok2.jpg");myOk = new Texture("ok2.jpg");
-		control = new Control(model);
+
 	}
 
 	@Override
 	public void render () {
-		update();
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		batch.begin();
-
-		batch.draw(myFon, 0, 0);
-		batch.draw(myOk, Gdx.graphics.getWidth()/6, Gdx.graphics.getHeight()*1/50);
-
-		model.render(batch);
-		batch.end();
+		super.render();
 	}
-	public void update() {
-
-		Vector2 v;
-
-		if (Input.isClicked()) {
-			v = Input.getMousePosition();
-			if (model.inSticks(v) != -1)
-			model.choiceStick(model.inSticks(v));
-
-			System.out.println("YES");
-
-		}
+	@Override
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
 }
 
